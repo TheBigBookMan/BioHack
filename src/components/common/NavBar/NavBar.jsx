af60be83df0cc307ebf74b0754d1b1fa30ce3731
Ben Smerd <bsmerd@adam.com.au>
2023-04-13 19:00:29 +0930

    add in hamburger menu

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -1,7 +1,12 @@
 const NavBar = () => {
     return (
-        <div className="flex justify-between">
-            <h1>BioHack</h1>
+        <div className="p-8 flex justify-between">
+            <h1 className="font-bold">BioHack</h1>
+            <ul className="flex flex-col relative group cursor-pointer">
+                <div className="absolute w-8 h-1 bg-black right-8 rounded-2xl"></div>
+                <div className="absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl"></div>
+                <div className="absolute w-8 h-1 bg-black right-8 top-4 rounded-2xl"></div>
+            </ul>
         </div>
     );
 };
