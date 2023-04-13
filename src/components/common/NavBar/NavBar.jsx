94037abcbe3dc6924c474f484078235765155ed2
Ben Smerd <bsmerd@adam.com.au>
2023-04-13 19:13:45 +0930

    more hamburger styling

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -6,10 +6,25 @@ const NavBar = () => {
     return (
         <div className="p-8 flex justify-between">
             <h1 className="font-bold">BioHack</h1>
-            <ul className="flex flex-col relative group cursor-pointer">
-                <div className="absolute w-8 h-1 bg-black right-8 rounded-2xl group-hover:rotate-45 group-hover:top-2 transition"></div>
-                <div className="absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl opacity-100  transition group-hover:left-12"></div>
-                <div className="absolute w-8 h-1 bg-black right-8 top-4 rounded-2xl group-hover:-rotate-45 group-hover:top-2 transition"></div>
+            <ul
+                onClick={() => setNavOpen(!navOpen)}
+                className="flex flex-col relative group cursor-pointer"
+            >
+                <div
+                    className={`absolute w-8 h-1 bg-black right-8 rounded-2xl ${
+                        navOpen ? "rotate-45 top-2" : ""
+                    }  transition`}
+                ></div>
+                <div
+                    className={`absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl ${
+                        navOpen ? "" : ""
+                    } opacity-100  transition `}
+                ></div>
+                <div
+                    className={`absolute w-8 h-1 bg-black right-8 top-4 rounded-2xl  ${
+                        navOpen ? "-rotate-45 -top-0" : ""
+                    } transition`}
+                ></div>
             </ul>
         </div>
     );
