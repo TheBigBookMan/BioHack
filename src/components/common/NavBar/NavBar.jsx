4b6e12e83088db873a8a526476b55dfaa257d221
Ben Smerd <bsmerd@adam.com.au>
2023-04-13 19:10:02 +0930

    hamburger menu styling

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 const NavBar = () => {
+    const [navOpen, setNavOpen] = useState(false);
+
     return (
         <div className="p-8 flex justify-between">
             <h1 className="font-bold">BioHack</h1>
             <ul className="flex flex-col relative group cursor-pointer">
-                <div className="absolute w-8 h-1 bg-black right-8 rounded-2xl"></div>
-                <div className="absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl"></div>
-                <div className="absolute w-8 h-1 bg-black right-8 top-4 rounded-2xl"></div>
+                <div className="absolute w-8 h-1 bg-black right-8 rounded-2xl group-hover:rotate-45 group-hover:top-2 transition"></div>
+                <div className="absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl opacity-100  transition group-hover:left-12"></div>
+                <div className="absolute w-8 h-1 bg-black right-8 top-4 rounded-2xl group-hover:-rotate-45 group-hover:top-2 transition"></div>
             </ul>
         </div>
     );
