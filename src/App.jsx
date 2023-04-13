9b285e0a6eb3d235cdf7464ce5be813603bf4dc8
Ben Smerd <bsmerd@adam.com.au>
2023-04-13 18:54:57 +0930

    add in components for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import NavBar from "./components/common/NavBar/NavBar";
+import Home from "./pages/Home";
+
+//TODO this will just be a landing page similar to the BioComputer cool website
+
+// todo use threejs to create some interaction
+//todo maybe try blender to create some cool models or something
+
+//TODO have some pages that have cool effects with transitions and interactivity
+
 function App() {
-  return <div className="font-bold">hey</div>;
+    return (
+        <div className="flex flex-col">
+            <NavBar />
+            <Home />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/common/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <div className="flex justify-between">
+            <h1>BioHack</h1>
+        </div>
+    );
+};
+
+export default NavBar;
