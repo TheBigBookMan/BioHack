4ba962eb6cc40b76d182c965095c509dccef660d
Ben Smerd <bsmerd@adam.com.au>
2023-04-14 12:00:41 +0930

    update hamburger menu

--- src/components/common/NavBar/NavBar.jsx
+++ src/components/common/NavBar/NavBar.jsx
@@ -4,25 +4,25 @@ const NavBar = () => {
     const [navOpen, setNavOpen] = useState(false);
 
     return (
-        <div className="p-8 flex justify-between">
+        <div className="p-8 flex justify-between items-center">
             <h1 className="font-bold">BioHack</h1>
             <ul
                 onClick={() => setNavOpen(!navOpen)}
-                className="flex flex-col relative group cursor-pointer"
+                className="flex flex-col justify-center w-[40px] h-[40px] relative group cursor-pointer"
             >
                 <div
-                    className={`absolute w-8 h-1 bg-black right-8 rounded-2xl ${
-                        navOpen ? "rotate-45 top-2" : ""
+                    className={`absolute w-8 h-1 bg-black top-2 rounded-2xl ${
+                        navOpen ? "rotate-45" : ""
                     }  transition`}
                 ></div>
                 <div
-                    className={`absolute w-6 h-1 bg-black right-8 top-2 rounded-2xl ${
-                        navOpen ? "" : ""
-                    } opacity-100  transition `}
+                    className={`absolute w-6 h-1 bg-black  rounded-2xl ${
+                        navOpen ? "opacity-0" : "opacity-100"
+                    }   transition `}
                 ></div>
                 <div
-                    className={`absolute w-8 h-1 bg-black right-8  rounded-2xl  ${
-                        navOpen ? "-rotate-45 " : "top-4"
+                    className={`absolute w-8 h-1 bg-black top-7 rounded-2xl  ${
+                        navOpen ? "-rotate-45" : ""
                     } transition`}
                 ></div>
             </ul>
