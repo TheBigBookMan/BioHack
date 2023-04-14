6f2f19e1998db09bf7a5a73195f862e633b9f25a
Ben Smerd <bsmerd@adam.com.au>
2023-04-14 12:38:56 +0930

    create slogan and heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="h-screen w-screen flex flex-col items-center justify-center p-4">
+            <div className="flex flex-col">
+                <h1 className="text-white tracking-widest">
+                    RE-ENGINEERING HUMAN MECHANICS
+                </h1>
+                <h1 className="font-bold text-white text-5xl">
+                    {" "}
+                    Unleashing Computational BioHacking
+                </h1>
+            </div>
         </div>
     );
 };
